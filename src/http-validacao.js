2bad0b158d6f543b4214f3e8df6bc0f7ea07dcbf
Bianca Ramos <biancakaroliner04@gmail.com>
2023-05-02 19:14:13 -0300

    Node

---
+++ src/http-validacao.js
@@ -0,0 +1,38 @@
+function extraiLinks(arrLinks){
+    //extrair somente a URL 
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
+}
+
+async function checaStatus(listaURLs){
+    const arrStatus = await Promise
+    .all(
+        listaURLs.map(async (url) => {
+            try {
+                const response = await fetch(url)
+                return response.status;
+            } catch (erro) {
+                return manejaErros(erro);
+            }
+        })
+    )
+    return arrStatus;
+}
+
+function manejaErros(erro){
+    if(erro.cause.code === 'ENOTFOUND'){
+        return 'link não encontrado';
+    } else {
+        return 'ocorreu algum erro';
+    }
+}
+
+export default async function listaValidada(listaDeLinks){
+   const links = extraiLinks(listaDeLinks);
+   const status = await checaStatus(links);
+
+   //abre parenteses de depois chaves para o js saber que é um objeto e não função
+   return listaDeLinks.map((objeto, indice) => ({
+    ...objeto, 
+    status: status[indice]
+   }))
+}
